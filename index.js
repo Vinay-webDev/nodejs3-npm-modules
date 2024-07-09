160ec8c035682a17fae58d5077963db22918722b
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-07-09 23:31:57 +0530

    1. nodemon not working:((((

--- index.js
+++ index.js
@@ -2,7 +2,9 @@
 
 console.log('testing');
 
-
+// installing nodemon 
+// node i nodemon -g
+// nodemon helps to run the code in node while having nodemon installed we don't have to write node filename in the terminal every single time. every time we press ctrl + s (save) nodemon will do node filename for us****
 
 
 
