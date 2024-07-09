64a344518b68afce6fdc1d21c721ab8e41d5f4d6
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-07-09 23:46:16 +0530

    3. node dependencies

--- index.js
+++ index.js
@@ -3,10 +3,12 @@
 console.log('testing');
 
 // installing nodemon 
-// node i nodemon -g
+// npm i nodemon -g
 // nodemon helps to run the code in node while having nodemon installed we don't have to write node filename in the terminal every single time. every time we press ctrl + s (save) nodemon will do node filename for us****
 
-
+// node dependecies have huge number of modules
+// so we don't want these files in github 
+// so add .gitignore file
 
 
 
